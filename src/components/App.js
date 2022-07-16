bcec839b9e9a3504e939a9d0908be1c4c1c15dea
Taras Bondar <strelezzzz@gmail.com>
2022-07-16 15:41:30 +0300

    update logic

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Feedback from './Feedback/Feedback';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = stateName => {
+    this.setState(prevState => ({
+      [stateName]: prevState[stateName] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const keys = Object.keys(this.state);
+    console.log(this.state);
+    console.log(this.props);
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const feedbackPercent = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <Feedback
+          keys={keys}
+          onHandleIncrement={this.handleIncrement}
+          total={total}
+          feedbackPercent={feedbackPercent}
+          good={good}
+          neutral={neutral}
+          bad={bad}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+// import PropTypes from 'prop-types';
+// import css from './Feedback.module.css';
+
+const Feedback = ({
+  keys,
+  onHandleIncrement,
+  good,
+  neutral,
+  bad,
+  total,
+  feedbackPercent,
+}) => (
+  <>
+    <h2>Please live feedback</h2>
+    <ul className="FeedbackListBtn">
+      {keys.map(key => {
+        return (
+          <button
+            key={key}
+            type="button"
+            onClick={() => onHandleIncrement(key)}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </ul>
+    <h2>Statistics</h2>
+    <ul className="FedbackListStat">
+      <li className="FeedbackList__item">Good: {good}</li>
+      <li className="FeedbackList__item">Neutral: {neutral}</li>
+      <li className="FeedbackList__item">Bad: {bad}</li>
+      <li className="FeedbackList__item">Total: {total}</li>
+      <li className="FeedbackList__item">feedbackPercent: {feedbackPercent}</li>
+    </ul>
+  </>
+);
+
+export default Feedback;
